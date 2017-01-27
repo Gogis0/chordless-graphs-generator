45efcdaec9518a91e27ba4deeeb55bf0835aad7f
Gogis0 <adriangoga0@gmail.com>
2017-01-27 17:05:46 +0100

    Fix output format and some bugs, clarify README

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 Compile as `g++ -W -Wall -g -O2 -std=c++11 find_cycles.cpp`.
 
 #### What it does:
-The program takes a set of circle lengths *X* and a an undirected Graph *G* as an input and:
+The program takes a set of circle lengths *X* and an undirected graph *G* as an input and:
 
-1. determines whether it contains chordless cycles of every length in *X*.
+1. Determines whether it contains a chordless cycle of such a length that is in *X*.
 
-2. determines whether it contains such an edge *e* that the first conditon doesn't hold for *G-e*.
+2. If it doesn't (i.e. the first condition isn't fulfilled), determines whether *G* contains an edge
+   *e* for which *G-e* fulfills condition 1. Such an edge we will call critical.
 
 #### Input format:
 The first line contains one positive integer *K*.
@@ -17,3 +18,9 @@ The next line contains *K* positive integers - the lengths of the chordless cycl
 On the next line there are two space separated integers *N* and *M*, *N* specifying the number of vertices of the graph *G* and *M* the number of edges.
 *M* lines follow, each in a format "X Y" meaning that there is a undirected edge from vertex *X* to vertex *Y*. Vertices are numbered from *0* to *N-1*.
 
+#### Output format:
+One line containing one of the 3 possible output messages, specifically:
+
++ "G contains a chordless cycle of length in X"
++ "G contains a critical edge"
++ "G doesn't contain a critical edge"
--- find_cycles.cpp
+++ find_cycles.cpp
@@ -2,9 +2,17 @@
 #include <algorithm>
 #include <vector>
 #include <set>
+#include <string>
 using namespace std;
 
+// max number of vertices
 const int MAX_N = 35;
+
+// output messages in one place, so changing them is easier
+const string out_has_cycle = "G contains a chordless cycle of length in X";
+const string out_has_critical = "G contains a critical edge";
+const string out_is_ok = "G doesn't contain a critical edge";
+
 
 set<int> find_chordless_cycles(bool adj[][MAX_N], int N) {
     set<int> cycles;
@@ -56,12 +64,18 @@ set<int> find_chordless_cycles(bool adj[][MAX_N], int N) {
     return cycles;
 }
 
+set<int> get_intersection(set<int> X, set<int> Y) {
+    set<int> intersect;
+    set_intersection(X.begin(), X.end(), Y.begin(), Y.end(), inserter(intersect, intersect.begin()));
+    return intersect;
+}
+
 pair<int, int> try_delete_edge(bool adj[][MAX_N], int N, set<int> X) {
     for (int i = 0; i < N - 1; i++) {
         for (int j = i + 1; j < N; j++) {
             if (adj[i][j]) {
                 adj[i][j] = adj[j][i] = false;
-                if (X != find_chordless_cycles(adj, N)) {
+                if (!get_intersection(X, find_chordless_cycles(adj, N)).empty()) {
 		    return {i, j};
                 }
                 adj[i][j] = adj[j][i] = true;
@@ -81,6 +95,8 @@ int main() {
 
     int N, M; cin >> N >> M;
     bool adj[MAX_N][MAX_N];
+    fill(adj[0], adj[0] + MAX_N*MAX_N, false);
+
     for (int i = 0; i < M; i++) {
         int x, y;
         cin >> x >> y;
@@ -89,10 +105,9 @@ int main() {
 
     set<int> found_cycles = find_chordless_cycles(adj, N);
 
-    if (X != find_chordless_cycles(adj, N)) {
-        cout << "Graf obsahuje vsetky kruznice s dlzkami z mnoziny X" << endl;
+    if (get_intersection(X, found_cycles).empty()) {
         auto res = try_delete_edge(adj, N, X);
-	if (res.first == -1) cout << "Graf neobsahuje hranu, ktorej odstranenim sa zmensi pocet indukovanych kruznic" << endl;
-	else cout << "Odstranenim hrany [" << res.first << "," << res.second << "]" << " sa zmensi pocet indukovanych cyklov" << endl;
-    } else cout << "Graf neobsahuje nejaku indukovanu kruznicu s dlzkou z mnoziny X" << endl;
+	if (res.first == -1) cout << out_is_ok << endl;
+	else cout << out_has_critical << endl;
+    } else cout << out_has_cycle << endl;
 }
